4529d0fd2d4b9829d226af7b47f67f7ff7b54ec7
Déborah Ballaminut <deborah.balla@gmail.com>
2021-09-11 20:23:54 -0300

    Inserindo validações

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+//Importação para utilizar formulários do Angular
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-tab1',
@@ -7,6 +9,19 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  constructor() {}
+  formCadastro: FormGroup;
+
+  constructor(public formBuilder: FormBuilder) {
+    //Montar a estrutura do Formulário
+    this.formCadastro = formBuilder.group({
+      //valor padrão entra na primeira aspas
+      nome: ['', Validators.compose([Validators.required, Validators.minLength(3)])],
+      email: ['', Validators.compose([Validators.required, Validators.email])],
+      cpf: ['', Validators.compose([Validators.required])],
+      dataNascimento: ['', Validators.required],
+      senha: ['', Validators.compose([Validators.required, Validators.minLength(6), Validators.maxLength(8)])],
+      confirmaSenha: ['', Validators.compose([Validators.required, Validators.minLength(6), Validators.maxLength(8)])],
+    });
+  }
 
 }
