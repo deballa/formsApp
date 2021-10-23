15ebe48b823a4b6524ad5ec17cdc02cac0473244
Déborah Ballaminut <deborah.balla@gmail.com>
2021-10-22 22:43:24 -0300

    Validação ao excluir usuário

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,6 +1,7 @@
 import { StorageService } from './../services/storage.service';
 import { Component } from '@angular/core';
 import { Usuario } from '../models/Usuario';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab2',
@@ -11,7 +12,10 @@ export class Tab2Page {
 
   listaUsuarios: Usuario[] = [];
 
-  constructor(public storageService: StorageService) {}
+  constructor(
+    public storageService: StorageService,
+    public alertController: AlertController,
+    ) {}
 
   ionViewDidEnter() {
     this.buscarUsuarios();
@@ -24,6 +28,29 @@ export class Tab2Page {
   async removerUsuario(key: string){
     await this.storageService.remove(key);
     this.buscarUsuarios();
+  }
+
+  async exibirMensagem(key: string) {
+    const alert = await this.alertController.create({
+      header: 'Excluir',
+      message: 'Deseja remover o contato?',
+      buttons: [
+        {
+          text: 'Cancelar',
+          role: 'cancel',
+          handler: () => {
+            console.log('ação cancelada');
+          }
+        }, {
+          text: 'Sim',
+          handler: () => {
+            this.removerUsuario(key);
+          }
+        }
+      ]
+    });
+
+    await alert.present();
   }
 
 }
